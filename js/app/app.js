3b01ed79e23a5102c8fcad4ec9d163949864761c
warihanagumah <warihana.gumah@ashesi.edu.gh>
2017-10-12 12:36:42 +0000

    commit

--- js/app/app.js
+++ js/app/app.js
@@ -1,9 +1,12 @@
-// Wait for Cordova to load
+// Since we will be making use of 3rd party functions (eg. navigator.geolocation.getCurrentPosition) which are not native javascript functions we will need
+// to add this function to the list of native javascript functions to allow javascript identify and execute it each time its called.
+// This is done by using the addEventListener() function.
 //
 document.addEventListener("deviceready", onDeviceReady, false);
 
-// Cordova is ready
-//
+
+//We decide to create a function to handle the 3rd party functions (eg. navigator.geolocation.getCurrentPosition)
+// which we earlier added to the native functions of the javascript
 function onDeviceReady() {
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
@@ -11,15 +14,16 @@ function onDeviceReady() {
 // onSuccess Geolocation
 //
 function onSuccess(position) {
+
     var element = document.getElementById('geolocation');
-    element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+    element.innerHTML = 'Latitude: ' + position.coords.latitude  + '<br />' +
         'Longitude: '          + position.coords.longitude             + '<br />' +
         'Altitude: '           + position.coords.altitude              + '<br />' +
         'Accuracy: '           + position.coords.accuracy              + '<br />' +
         'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
         'Heading: '            + position.coords.heading               + '<br />' +
         'Speed: '              + position.coords.speed                 + '<br />' +
-        'Timestamp: '          +                                   position.timestamp          + '<br />';
+        'Timestamp: '          + position.timestamp          + '<br />';
 }
 
 // onError Callback receives a PositionError object
